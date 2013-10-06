5c324d31c7fff6c9ec9835ffe67014b4e08e7194
Steven Allen <steven@stebalien.com>
2013-10-06 10:34:44 -0400

    Simplify build script.

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-BACKGROUND=(src/ts/background.ts)
-ENCRYPT_FRAME=(src/ts/trusted_input.ts)
-DECRYPT_FRAME=()
-
-tsc --out src/js/background.js ${BACKGROUND[@]}
-tsc --out src/js/encrypt_frame.js ${ENCRYPT_FRAME[@]}
-tsc --out src/js/decrypt_frame.js ${DECRYPT_FRAME[@]}
+tsc --out src/js/ src/ts/*.ts
